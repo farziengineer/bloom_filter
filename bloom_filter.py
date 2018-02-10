358a87a947f82beb350d96eb9b1dafc118444cd2
Mohit Anand <anand.mohit001@gmail.com>
2018-02-10 19:09:09 +0530

    added checker for presence in filter

--- bloom_filter.py
+++ bloom_filter.py
@@ -1,6 +1,6 @@
 import mmh3
 from bitarray import bitarray
-
+import csv
 
 size=10
 inc_by=2
@@ -46,4 +46,56 @@ def extender(cur_size):
 	for idx, bits in enumerate(extra):
 		bit_array[2*idx] = extra[idx]
 
+
+
+def checker(url):
+	bit_1 = mmh3.hash(url, 21)%size
+	bit_array[bit_1]=1
+
+	bit_2 = mmh3.hash(url, 22)%size
+	bit_array[bit_2]=1
+
+	bit_3 = mmh3.hash(url, 23)%size
+	bit_array[bit_3]=1
+
+	bit_4 = mmh3.hash(url, 24)%size
+	bit_array[bit_4]=1
+
+	bit_5 = mmh3.hash(url, 25)%size
+	bit_array[bit_5]=1
+
+	bit_6 = mmh3.hash(url, 26)%size
+	bit_array[bit_6]=1
+
+	bit_7 = mmh3.hash(url, 27)%size
+	bit_array[bit_7]=1
+
+	if bit_array[bit_1] and bit_array[bit_2] \
+	 and bit_array[bit_3] and bit_array[bit_4]
+	  and bit_array[bit_5] and bit_array[bit_6]
+	   and bit_array[bit_7]:
+	   return "maybe present"
+	   
+    return "defenitely not present" 
+
+
+
+def reader_and_writer():
+	r = csv.reader(open("top1m.csv"));
+	for row in r:
+		url=row[1]
+		mapper(url);
+
+
+	f=open("bloom_filter.txt","wb")
+	f.write(bit_array)
+	f.close()
+
+
+reader_and_writer()
+
+
+# while(1):
+# 	n=int(raw_input())
+# 	if(n==1):
 
